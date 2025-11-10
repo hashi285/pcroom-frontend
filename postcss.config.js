9de51b9a113fe0e546420b34ad43fb9a9e1cc712
suhyeok moon <m51187251@gmail.com>
2025-11-10 14:16:09 +0900

    수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Noto Sans KR', 'sans-serif'], // ✅ 추가
-      },
-    },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
-}
+};
